8ff0ae4d4d5d5ca0529ba27443efff9799b4556e
Ilia Juravsky <ilia.jurav@gmail.com>
2020-09-13 16:26:06 +0300

    Решен первый урок 13.09.2020 (убраны типы функций)

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -69,7 +69,7 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = seconds + 60 * minutes + 3600 * hours
+fun seconds(hours: Int, minutes: Int, seconds: Int) = seconds + 60 * minutes + 3600 * hours
 
 /**
  * Тривиальная (1 балл)
@@ -78,8 +78,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = seconds + 60 * minute
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int):
-        Double = ((sagenes * 3 + arshins) * 16 + vershoks) * 4.445 / 100.0
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int) =
+    ((sagenes * 3 + arshins) * 16 + vershoks) * 4.445 / 100.0
 
 /**
  * Тривиальная (1 балл)
@@ -87,7 +87,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int):
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (deg + (min + sec / 60.0) / 60.0) / 180.0 * PI
+fun angleInRadian(deg: Int, min: Int, sec: Int) = (deg + (min + sec / 60.0) / 60.0) / 180.0 * PI
 
 /**
  * Тривиальная (1 балл)
@@ -95,7 +95,7 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (deg + (min + sec / 60
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(y2 - y1) + sqr(x2 - x1))
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double) = sqrt(sqr(y2 - y1) + sqr(x2 - x1))
 
 /**
  * Простая (2 балла)
@@ -103,7 +103,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(s
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = (number / 100) % 10
+fun thirdDigit(number: Int) = (number / 100) % 10
 
 /**
  * Простая (2 балла)
@@ -112,8 +112,8 @@ fun thirdDigit(number: Int): Int = (number / 100) % 10
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int):
-        Int = (60 * hoursArrive + minutesArrive) - (60 * hoursDepart + minutesDepart)
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int) =
+    (60 * hoursArrive + minutesArrive) - (60 * hoursDepart + minutesDepart)
 
 /**
  * Простая (2 балла)
@@ -122,7 +122,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = initial * (1 + percent / 100.0).pow(3)
+fun accountInThreeYears(initial: Int, percent: Int) = initial * (1 + percent / 100.0).pow(3)
 
 /**
  * Простая (2 балла)
@@ -130,4 +130,4 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = initial * (1 + per
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = (number / 100) + ((number / 10) % 10) * 10 + (number % 10) * 100
+fun numberRevert(number: Int) = (number / 100) + ((number / 10) % 10) * 10 + (number % 10) * 100
