6382ba688492074dabdb9478e0bb9bfe7d7b6c4e
Ilia Juravsky <ilia.jurav@gmail.com>
2020-11-18 12:27:41 +0300

    18.11.2020 одиннадцатая отправка lesson 5,6

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -350,7 +350,26 @@ fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
  */
 
 fun bagPacking(treasures: Map<String, Pair<Int, Int>>, capacity: Int): Set<String> {
-    return setOf(
-        "cap=$capacity, tr_size=${treasures.count()}"
-    )
+    val curRow = mutableListOf<Triple<Int, Int, Set<String>>>()
+    val prevRow = mutableListOf(Triple(0, 0, emptySet<String>()))
+    treasures.forEach { name, (weight, cost) ->
+        prevRow.forEach { (load, total, set) ->
+            if (load + weight <= capacity)
+                curRow.add(Triple(load + weight, total + cost, set + name))
+        }
+        curRow.addAll(prevRow)
+        curRow.sortBy { it.first }
+        var maxTotal = -1
+        val filteredRow = curRow.filter { (_, total, _) ->
+            if (maxTotal >= total) {
+                false
+            } else {
+                maxTotal = total;true
+            }
+        }
+        prevRow.clear()
+        prevRow.addAll(filteredRow)
+        curRow.clear()
+    }
+    return prevRow.last().third
 }
